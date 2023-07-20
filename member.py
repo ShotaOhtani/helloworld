10b8516277a4ba49917ff0cb1aed7b6b9c7515c7
ShotaOhtani <shotigger0508@gmail.com>
2023-07-20 11:36:23 +0900

    ver2に変更

--- member.py
+++ member.py
@@ -1,4 +1,4 @@
-ver = "0.2"
+ver = "0.3"
 
 class Member:
     def __init__(self, name, words=""):
@@ -18,7 +18,8 @@ mlist.append(newmember)
 
 
 ### 以下に自分を追加する ###
-newmember = Member("大河原翔太", "よろしくお願いいたします。")
+mlist = []
+newmember = Member("大谷渉太", "shota！")
 mlist.append(newmember)
 
 
