da7b8a9d7b14db09268f132ceeaa2b5ca732e4d4
ShotaOhtani <shotigger0508@gmail.com>
2023-08-02 15:32:15 +0900

    Update member.py

--- member.py
+++ member.py
@@ -71,7 +71,7 @@ mlist.append(newmember)
 newmember = Member("小犬丸 愛花", "とても眠いです")
 mlist.append(newmember)
 
-newmember = Member("大谷渉太", "バモス!!")
+newmember = Member("大谷渉太", "バモス!")
 mlist.append(newmember)
 
 
